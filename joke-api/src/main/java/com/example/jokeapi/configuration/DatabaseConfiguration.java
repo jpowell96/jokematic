1b1298e1dc4dc44f75de97c1825f444a701a16bb
Jonathan Powell <powell.jonj@gmail.com>
2023-07-10 14:13:26 -0400

    Refactor to read from environment variables

--- joke-api/src/main/java/com/example/jokeapi/configuration/DatabaseConfiguration.java
+++ joke-api/src/main/java/com/example/jokeapi/configuration/DatabaseConfiguration.java
@@ -2,6 +2,8 @@ package com.example.jokeapi.configuration;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,18 +12,14 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
+@EnableConfigurationProperties
 public class DatabaseConfiguration {
     
     @Bean
+    @ConfigurationProperties(prefix="joke.db.datasource")
     public DataSource postgresJokeDataSource() {
-        // TODO: Read URL, Username, and Password from application.properties or from external source (AWS Secrets Manager)
-        return DataSourceBuilder.create()
-        .url("jdbc:postgresql://localhost:5432/joke_db")
-        .username("postgres")
-        .password(null)
-        .driverClassName("org.postgresql.Driver")
-        .type(HikariDataSource.class)
-        .build();
+        // DataSource autopopulates jdbcUrl, user, and password from the properties file, which pulls from env variables.
+        return DataSourceBuilder.create().build();
     }
 
     @Bean
